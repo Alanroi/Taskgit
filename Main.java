5cd34508de04120fe67d72b01355e97273df6de6
Alan Roi <alanroira@gmail.com>
2022-07-06 17:30:44 -0500

    adding sub, div, mult and sqrt methods

--- Main.java
+++ Main.java
@@ -6,5 +6,17 @@ public class Main {
     public int add(int a, int b){
         return a+b;
     }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
+    public int mult(int a, int b){
+        return a*b;
+    }
+    public double sqrt(int a){
+        return Math.sqrt(a);
+    }
     
 }
